d3af620e7dfa4422fa22d06f7e9c8937544cff05
Eck Doerry <eckdoerry@gmail.com>
2012-02-08 16:28:21 -0700

    Added a sound component to middle button to demo sound resources

--- src/components/MediaPackageDemo.java
+++ src/components/MediaPackageDemo.java
@@ -29,30 +29,34 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */ 
 
+// Modified by Eck Doerry 2/2012, to show packaging of image and sound resources, playable in runnable jar.
+// Requires image files within an "image" subfolder inside the package
+// Requires sound files inside a "sounds" folder" inside the project package.
+
 package components;
 
-import javax.swing.AbstractButton;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JFrame;
-import javax.swing.ImageIcon;
-
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
-/* 
- * ButtonDemo.java requires the following files:
- *   images/right.gif
- *   images/middle.gif
- *   images/left.gif
- */
-public class ButtonDemo extends JPanel
+import javax.swing.AbstractButton;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class MediaPackageDemo extends JPanel
                         implements ActionListener {
     protected JButton b1, b2, b3;
     static final long serialVersionUID=1;
+    private AudioClip beepSound;
+   
 
-    public ButtonDemo() {
+    public MediaPackageDemo() {
         ImageIcon leftButtonIcon = createImageIcon("images/right.gif");
         ImageIcon middleButtonIcon = createImageIcon("images/middle.gif");
         ImageIcon rightButtonIcon = createImageIcon("images/left.gif");
@@ -63,10 +67,11 @@ public class ButtonDemo extends JPanel
         b1.setMnemonic(KeyEvent.VK_D);
         b1.setActionCommand("disable");
 
-        b2 = new JButton("Middle button", middleButtonIcon);
+        b2 = new JButton("Giddy-up pardner!!", middleButtonIcon);
         b2.setVerticalTextPosition(AbstractButton.BOTTOM);
         b2.setHorizontalTextPosition(AbstractButton.CENTER);
         b2.setMnemonic(KeyEvent.VK_M);
+        b2.setActionCommand("doAction");
 
         b3 = new JButton("Enable middle button", rightButtonIcon);
         //Use the default text position of CENTER, TRAILING (RIGHT).
@@ -74,37 +79,58 @@ public class ButtonDemo extends JPanel
         b3.setActionCommand("enable");
         b3.setEnabled(false);
 
-        //Listen for actions on buttons 1 and 3.
+        //Listen for actions on buttons 1 and 3.  // Eck: Listens on button2 now too
         b1.addActionListener(this);
         b3.addActionListener(this);
+        b2.addActionListener(this);
 
         b1.setToolTipText("Click this button to disable the middle button.");
-        b2.setToolTipText("This middle button does nothing when you click it.");
+        b2.setToolTipText("This middle button plays a sound when you click it.");
         b3.setToolTipText("Click this button to enable the middle button.");
 
         //Add Components to this container, using the default FlowLayout.
         add(b1);
         add(b2);
         add(b3);
+        
+        // Finally, load up the sound clip so it's ready to play;
+        beepSound = createAudioClip("sounds/cowboy.wav");
     }
 
     public void actionPerformed(ActionEvent e) {
-        if ("disable".equals(e.getActionCommand())) {
-            b2.setEnabled(false);
-            b1.setEnabled(false);
-            b3.setEnabled(true);
-        } else {
-            b2.setEnabled(true);
-            b1.setEnabled(true);
-            b3.setEnabled(false);
-        }
+    	if ("disable".equals(e.getActionCommand())) {
+    		b2.setEnabled(false);
+    		b1.setEnabled(false);
+    		b3.setEnabled(true);
+    	} else if ("doAction".equals(e.getActionCommand())) {
+    		if (beepSound != null) {
+    			beepSound.play();
+    		} else {
+    			Toolkit.getDefaultToolkit().beep();
+    		}
+    	} else {
+    		b2.setEnabled(true);
+    		b1.setEnabled(true);
+    		b3.setEnabled(false);
+    	}
     }
 
     /** Returns an ImageIcon, or null if the path was invalid. */
     protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = ButtonDemo.class.getResource(path);
+        java.net.URL imgURL = MediaPackageDemo.class.getResource(path);
         if (imgURL != null) {
             return new ImageIcon(imgURL);
+        } else {
+        	System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+    
+    /** Returns an new AudioClip, or null if the path was invalid. */
+    protected static AudioClip createAudioClip(String path) {
+        java.net.URL clipURL = MediaPackageDemo.class.getResource(path);
+        if (clipURL != null) {
+            return Applet.newAudioClip(clipURL);
         } else {
         	System.err.println("Couldn't find file: " + path);
             return null;
@@ -123,7 +149,7 @@ public class ButtonDemo extends JPanel
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Create and set up the content pane.
-        ButtonDemo newContentPane = new ButtonDemo();
+        MediaPackageDemo newContentPane = new MediaPackageDemo();
         newContentPane.setOpaque(true); //content panes must be opaque
         frame.setContentPane(newContentPane);
 
